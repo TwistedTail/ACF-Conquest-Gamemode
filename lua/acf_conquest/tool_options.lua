91f96b2770233b162558a244bf6dbeffc489467a
TwistedTail <nachosky24@gmail.com>
2019-06-12 21:31:09 -0400

    Improved method for creating menu items
    - Added AddOption and AddOptionItem functions in order to make adding options on the tool's menu an easier job.
    - Globalized CreateTempItem, AddOption and AddOptionItem functions.
    - Modified the menu reload button to include the new changes.
    - Added a button on the WIPText button (that does nothing but count).

--- lua/acf_conquest/tool_options.lua
+++ lua/acf_conquest/tool_options.lua
@@ -1,4 +1,9 @@
+local CurrentOption
+local OptionsLookup = {}
+local OptionsTable = {}
+
 -- Adding compatibility with DForm panels
+-- https://wiki.garrysmod.com/page/Category:DForm
 local FormPanels = {
 	Button = { "", "", "" },
 	CheckBox = { "", "" },
@@ -12,7 +17,7 @@ local FormPanels = {
 	TextEntry = { "", "" }
 }
 
--- Don't use this function inside OptionsTable, use CreateTempItem instead!
+-- Don't use this function inside an Option Item, use CreateTempItem instead!
 local function CreateItem(Name, Parent, HasText)
 	local Panel
 	local FormData = FormPanels[Name]
@@ -48,7 +53,7 @@ local function CreateItem(Name, Parent, HasText)
 	return Panel
 end
 
--- I highly recommend to use this one for items created inside OptionsTable
+-- I highly recommend to use this one for items created inside AddOptionItem
 local function CreateTempItem(Name, Parent, HasText)
 	local Panel = CreateItem(Name, Parent, HasText)
 
@@ -71,150 +76,47 @@ end
 local function WIPText(Panel)
 	local Text = CreateTempItem("DLabel", Panel, true)
 	Text:SetText("Work in Progress.")
+
+	local Count = 0
+	local CoolButton = CreateTempItem("Button", Panel, true)
+	CoolButton:SetText("Here, press this button.")
+	CoolButton.DoClickInternal = function()
+		Count = Count + 1
+
+		CoolButton:SetText("Pressed " .. Count .. " time" .. (Count ~= 1 and "s." or "."))
+	end
 end
 
-local OptionsTable = {
-	{
-		name = "About the Addon",
-		data = {
-			{
-				name = "Updates",
-				icon = "icon16/newspaper.png",
-				action = WIPText,
-			},
-			{
-				name = "Description",
-				icon = "icon16/information.png",
-				action = WIPText,
-			},
-			{
-				name = "Report a Bug",
-				icon = "icon16/bug.png",
-				action = function(Panel)
-					local Text = CreateTempItem("Help", Panel, true)
-					Text:SetText("Found a bug and don't really want to fix it yourself?\n" ..
-								"Leave an issue report on the Github page!")
+local function AddOption(Name, SuperOnly)
+	if not Name then return end
 
-					local GitButton = CreateTempItem("Button", Panel, true)
-					GitButton:SetText("Report a Bug Now!")
-					GitButton.DoClick = function()
-						gui.OpenURL("https://github.com/TwistedTail/ACF-Conquest-Gamemode/issues/new/choose")
-					end
+	local Index = #OptionsTable + 1
 
-					local Note = CreateTempItem("Help", Panel, true)
-					Note:SetText("Important: Problems are easier to solve if you know what caused them. " ..
-								"It's highly suggested to provide information about the bug you're reporting.")
+	CurrentOption = Name
 
-					local Examples = CreateTempItem("Help", Panel, true)
-					Examples:SetText("Examples of 'useful information' would be:\n" ..
-								"- What were you doing when it happened?\n" ..
-								"- Was there an error on the console? If so, please provide it.\n" ..
-								"- Can you explain how to replicate the bug? If so, please give a short explanation.")
-				end,
-			},
-		},
-	},
-	{
-		name = "Tutorials",
-		data = {
-		},
-	},
-	{
-		name = "Server Settings",
-		superonly = true,
-		data = {
-			{
-				name = "Global Settings",
-				icon = "icon16/world.png",
-				action = function(Panel)
-					local Test = CreateTempItem("Help", Panel, true)
-					Test:SetText("It's not a soldier's gun (Not a soldier's gun!). " ..
-								"Tight tolerances? Full length guide rod? UNRELIABLE!? " ..
-								"IT'S UNRELIABLE LADIES AND GENTS! UN-FUCKING-RELIABLE " ..
-								"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	OptionsLookup[Name] = Index
+	OptionsTable[Index] = {
+		name = Name,
+		superonly = SuperOnly,
+		data = {},
+	}
+end
 
-					local GMEnable = CreateTempItem("CheckBox", Panel, true)
-					GMEnable:SetText("Enable the gamemode.")
-					GMEnable:SetConVar("acf_conquest_enable")
+local function AddOptionItem(Name, Icon, SuperOnly, Action)
+	if not CurrentOption then return end
+	if not OptionsLookup[CurrentOption] then return end
+	if not Name then return end
 
-					local BotEnable = CreateTempItem("CheckBox", Panel, true)
-					BotEnable:SetText("Enable bot spawning.")
-					BotEnable:SetConVar("acf_conquest_enable_bots")
+	local Index = OptionsLookup[CurrentOption]
+	local Data = OptionsTable[Index].data
 
-					local BotAmount = CreateTempItem("NumSlider", Panel)
-					BotAmount:SetTooltip("Defines the maximum amount of bots per team.")
-					BotAmount:SetText("Bots per team")
-					BotAmount:SetMax(100)
-					BotAmount:SetConVar("acf_conquest_max_bots")
-
-					local ScoreAmount = CreateTempItem("NumSlider", Panel)
-					ScoreAmount:SetTooltip("Defines the amount of tickets each team starts with.")
-					ScoreAmount:SetText("Tickets per team")
-					ScoreAmount:SetMinMax(100, 2000)
-					ScoreAmount:SetConVar("acf_conquest_max_tickets")
-				end
-			},
-			{
-				name = "Map Settings",
-				icon = "icon16/map.png",
-				action = WIPText,
-			},
-		},
-	},
-	{
-		name = "Team Settings",
-		superonly = true,
-		data = {
-			{
-				name = "View Teams",
-				icon = "icon16/eye.png",
-				action = WIPText,
-			},
-			{
-				name = "Edit Teams",
-				icon = "icon16/pencil.png",
-				action = WIPText,
-			}
-		},
-	},
-	{
-		name = "Capture Points",
-		superonly = true,
-		data = {
-			{
-				name = "View Capture Points",
-				icon = "icon16/eye.png",
-				action = WIPText,
-			},
-			{
-				name = "Edit Capture Points",
-				icon = "icon16/pencil.png",
-				action = WIPText,
-			},
-			{
-				name = "Order Capture Points",
-				icon = "icon16/arrow_switch.png",
-				action = WIPText,
-			},
-		},
-	},
-	{
-		name = "Spawn Points",
-		superonly = true,
-		data = {
-			{
-				name = "View Spawn Points",
-				icon = "icon16/eye.png",
-				action = WIPText,
-			},
-			{
-				name = "Edit Spawn Points",
-				icon = "icon16/pencil.png",
-				action = WIPText,
-			},
-		},
-	},
-}
+	Data[#Data + 1] = {
+		name = Name,
+		icon = Icon,
+		superonly = SuperOnly,
+		action = Action,
+	}
+end
 
 local function CanUseItem(Item, IsSuper)
 	if not Item then return false end
@@ -224,6 +126,8 @@ local function CanUseItem(Item, IsSuper)
 end
 
 local function GenerateMenu(ComboBox)
+	if not next(OptionsTable) then return end
+
 	local IsSuper = LocalPlayer():IsSuperAdmin()
 
 	for _, v in pairs(OptionsTable) do
@@ -273,7 +177,7 @@ local function CreateContextPanel(Panel)
 	local Reload = CreateItem("Button", Panel, true)
 	Reload:SetText("Press to reload menu.")
 	Reload.DoClick = function()
-		CreateContextPanel(Panel)
+		ACF_Conq.CreateContextPanel(Panel)
 	end
 
 	local Title = CreateItem("Help", Panel, true)
@@ -308,5 +212,80 @@ local function CreateContextPanel(Panel)
 	GenerateMenu(Options)
 end
 
+-- Filling the tool's menu
+AddOption("About the Addon")
+AddOptionItem("Updates", "icon16/newspaper.png", nil, WIPText)
+AddOptionItem("Description", "icon16/information.png", nil, WIPText)
+AddOptionItem("Report a Bug", "icon16/bug.png", nil, function(Panel)
+	local Text = CreateTempItem("Help", Panel, true)
+	Text:SetText("Found a bug and don't really want to fix it yourself?\n" ..
+				"Leave an issue report on the Github page!")
+
+	local GitButton = CreateTempItem("Button", Panel, true)
+	GitButton:SetText("Report a Bug Now!")
+	GitButton.DoClickInternal = function()
+		gui.OpenURL("https://github.com/TwistedTail/ACF-Conquest-Gamemode/issues/new/choose")
+	end
+
+	local Note = CreateTempItem("Help", Panel, true)
+	Note:SetText("Important: Problems are easier to solve if you know what caused them. " ..
+				"It's highly suggested to provide information about the bug you're reporting.")
+
+	local Examples = CreateTempItem("Help", Panel, true)
+	Examples:SetText("Examples of 'useful information' would be:\n" ..
+					"- What were you doing when it happened?\n" ..
+					"- Was there an error on the console? If so, please provide it.\n" ..
+					"- Can you explain how to replicate the bug? If so, please give a short explanation.")
+end)
+
+AddOption("Tutorials")
+
+AddOption("Server Settings", true)
+AddOptionItem("Global Settings", "icon16/world.png", nil, function(Panel)
+	local Test = CreateTempItem("Help", Panel, true)
+	Test:SetText("It's not a soldier's gun (Not a soldier's gun!). " ..
+				"Tight tolerances? Full length guide rod? UNRELIABLE!? " ..
+				"IT'S UNRELIABLE LADIES AND GENTS! UN-FUCKING-RELIABLE " ..
+				"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+	local GMEnable = CreateTempItem("CheckBox", Panel, true)
+	GMEnable:SetText("Enable the gamemode.")
+	GMEnable:SetConVar("acf_conquest_enable")
+
+	local BotEnable = CreateTempItem("CheckBox", Panel, true)
+	BotEnable:SetText("Enable bot spawning.")
+	BotEnable:SetConVar("acf_conquest_enable_bots")
+
+	local BotAmount = CreateTempItem("NumSlider", Panel)
+	BotAmount:SetTooltip("Defines the maximum amount of bots per team.")
+	BotAmount:SetText("Bots per team")
+	BotAmount:SetMax(100)
+	BotAmount:SetConVar("acf_conquest_max_bots")
+
+	local ScoreAmount = CreateTempItem("NumSlider", Panel)
+	ScoreAmount:SetTooltip("Defines the amount of tickets each team starts with.")
+	ScoreAmount:SetText("Tickets per team")
+	ScoreAmount:SetMinMax(100, 2000)
+	ScoreAmount:SetConVar("acf_conquest_max_tickets")
+end)
+AddOptionItem("Map Settings", "icon16/map.png", nil, WIPText)
+
+AddOption("Team Settings", true)
+AddOptionItem("View Teams", "icon16/eye.png", nil, WIPText)
+AddOptionItem("Edit Teams", "icon16/pencil.png", nil, WIPText)
+
+AddOption("Capture Point Settings", true)
+AddOptionItem("View Capture Points", "icon16/eye.png", nil, WIPText)
+AddOptionItem("Edit Capture Points", "icon16/pencil.png", nil, WIPText)
+AddOptionItem("Order Capture Points", "icon16/arrow_switch.png", nil, WIPText)
+
+AddOption("Spawn Point Settings", true)
+AddOptionItem("View Spawn Points", "icon16/eye.png", nil, WIPText)
+AddOptionItem("Edit Spawn Points", "icon16/pencil.png", nil, WIPText)
+
+-- Globalizing useful functions
 ACF_Conq.CreateContextPanel = CreateContextPanel
 ACF_Conq.CreateItem = CreateItem
+ACF_Conq.CreateTempItem = CreateTempItem
+ACF_Conq.AddOption = AddOption
+ACF_Conq.AddOptionItem = AddOptionItem
