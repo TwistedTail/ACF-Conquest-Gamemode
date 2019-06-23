17c1963872cd38048d98b48770253d3f61a0c961
TwistedTail <nachosky24@gmail.com>
2019-06-23 06:45:15 -0400

    Replaced indentation on util.lua
    - Replaced indentation on util.lua with tabs in order to follow the same format as the other files.

--- lua/acf_conquest/util.lua
+++ lua/acf_conquest/util.lua
@@ -4,21 +4,21 @@ ACSR_SET = 2
 ACSR_GETSET = 3
 
 local function CreateAccessor(Table, Key, Name, Type)
-    if not Table then return end
+	if not Table then return end
 
-    local Binary = math.IntToBin(Type)
+	local Binary = math.IntToBin(Type)
 
-    if bit.band("001", Binary) == 1 then
-        Table["Get" .. Name] = function(This)
-            return This[Key]
-        end
-    end
+	if bit.band("001", Binary) == 1 then
+		Table["Get" .. Name] = function(This)
+			return This[Key]
+		end
+	end
 
-    if bit.band("010", Binary) == 2 then
-        Table["Set" .. Name] = function(This, Value)
-            This[Key] = Value
-        end
-    end
+	if bit.band("010", Binary) == 2 then
+		Table["Set" .. Name] = function(This, Value)
+			This[Key] = Value
+		end
+	end
 end
 
 ACF_Conq.CreateAccessor = CreateAccessor
