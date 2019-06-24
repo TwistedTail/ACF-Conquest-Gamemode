85674432570c7c6b2746763034229ef49e5a9106
TwistedTail <nachosky24@gmail.com>
2019-06-23 20:16:17 -0400

    Removed unnecessary binary convertion
    - Remove unnecessary binary convertion inside CreateAccessor

--- lua/acf_conquest/util.lua
+++ lua/acf_conquest/util.lua
@@ -6,15 +6,13 @@ ACSR_GETSET = 3
 local function CreateAccessor(Table, Key, Name, Type)
 	if not Table then return end
 
-	local Binary = math.IntToBin(Type)
-
-	if bit.band("001", Binary) == 1 then
+	if bit.band(1, Type) == 1 then
 		Table["Get" .. Name] = function(This)
 			return This[Key]
 		end
 	end
 
-	if bit.band("010", Binary) == 10 then
+	if bit.band(2, Type) == 2 then
 		Table["Set" .. Name] = function(This, Value)
 			This[Key] = Value
 		end
