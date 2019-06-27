574acb5dc966c871c51c32c8060b75dc4e11c335
TwistedTail <nachosky24@gmail.com>
2019-06-27 10:39:07 -0400

    Improved tool menu functions
    - Improved tool menu functions, now every custom item we're gonna be using is loaded onto DForm's methods.
    - Loaded ClearItems onto DForm's methods
    - Removed CreateItem and CreateTempItem
    - Changed font to DermaDefaultBold

--- lua/acf_conquest/tool_options.lua
+++ lua/acf_conquest/tool_options.lua
@@ -2,99 +2,20 @@ local CurrentOption
 local OptionsLookup = {}
 local OptionsTable = {}
 
--- Adding compatibility with DForm panels
--- https://wiki.garrysmod.com/page/Category:DForm
-local FormPanels = {
-	Button = { "", "", "" },
-	CheckBox = { "", "" },
-	ComboBox = { "", "" },
-	ControlHelp = { "" },
-	Help = { "" },
-	ListBox = { "" },
-	NumberWang = { "", "", 0, 0, 0 },
-	NumSlider = { "", "", 0, 0, 0 },
-	PanelSelect = { },
-	TextEntry = { "", "" }
-}
-
--- Don't use this function inside an Option Item, use CreateTempItem instead!
-local function CreateItem(Name, Parent, HasText)
-	local Panel
-	local FormData = FormPanels[Name]
-
-	if FormData then
-		Panel = Parent[Name](Parent, unpack(FormData)) -- Creating a DForm panel. Ex: DForm.Button(DForm, ...)
-
-		if HasText then
-			Panel:SetFont("Trebuchet18")
-		end
-	else
-		Panel = vgui.Create(Name, Parent)
-		Panel:Dock(TOP)
-
-		if HasText then
-			Panel:SetFont("Trebuchet18")
-			Panel:SetDark(true)
-
-			if Panel.SetAutoStretchVertical then
-				Panel:SetAutoStretchVertical(true)
-			end
-
-			Panel:SetWrap(true)
-		end
-
-		Parent:AddItem(Panel)
-	end
-
-	Panel:DockMargin(5, 0, 5, 0)
-
-	Parent.CreatedItems[Panel] = true
-
-	return Panel
-end
-
--- I highly recommend to use this one for items created inside AddOptionItem
-local function CreateTempItem(Name, Parent, HasText)
-	local Panel = CreateItem(Name, Parent, HasText)
-
-	Parent.TempItems[Panel] = true
-
-	return Panel
-end
-
-local function ClearItems(Panel, Name)
-	local Items = Panel[Name]
-
-	if not Items then Panel[Name] = {} return end
-	if not next(Items) then return end
-
-	for Item in pairs(Items) do
-		Item:Remove()
-	end
-end
-
-local function WIPText(Panel)
-	local Text = CreateTempItem("DLabel", Panel, true)
-	Text:SetText("Work in Progress.")
-
-	local Count = 0
-	local CoolButton = CreateTempItem("Button", Panel, true)
-	CoolButton:SetText("Here, press this button.")
-	CoolButton.DoClickInternal = function()
-		Count = Count + 1
-
-		CoolButton:SetText("Pressed " .. Count .. " time" .. (Count ~= 1 and "s." or "."))
-	end
-end
-
 local function AddOption(Name, SuperOnly)
 	if not Name then return end
 
-	local Index = #OptionsTable + 1
+	local Index
+
+	if not OptionsLookup[Name] then
+		Index = #OptionsTable + 1
+		OptionsLookup[Name] = { Index = Index }
+	else
+		Index = OptionsLookup[Name].Index
+	end
 
 	CurrentOption = Name
 
-	OptionsLookup[Name] = Index
 	OptionsTable[Index] = {
 		name = Name,
 		superonly = SuperOnly,
@@ -107,15 +28,32 @@ local function AddOptionItem(Name, Icon, SuperOnly, Action)
 	if not OptionsLookup[CurrentOption] then return end
 	if not Name then return end
 
-	local Index = OptionsLookup[CurrentOption]
+	local Option = OptionsLookup[CurrentOption]
+	local Index = Option.Index
 	local Data = OptionsTable[Index].data
+	local DataIndex = Option[Name] or #Data + 1
 
-	Data[#Data + 1] = {
+	Option[Name] = DataIndex
+	Data[DataIndex] = {
 		name = Name,
 		icon = Icon,
 		superonly = SuperOnly,
 		action = Action,
 	}
+end
+
+local function WIPText(Panel)
+	local Text = Panel:FormTitle("Work in Progress.")
+	Text:SetTemporal(Panel)
+
+	local Count = 0
+	local CoolButton = Panel:FormButton("Here, press this button.", "", "")
+	CoolButton:SetTemporal(Panel)
+	CoolButton.DoClickInternal = function()
+		Count = Count + 1
+
+		CoolButton:SetText("Pressed " .. Count .. " time" .. (Count ~= 1 and "s." or "."))
+	end
 end
 
 local function CanUseItem(Item, IsSuper)
@@ -172,27 +110,24 @@ local function CreateContextPanel(Panel)
 	local Category
 	local Option
 
-	ClearItems(Panel, "CreatedItems")
+	Panel:ClearItems("CreatedItems")
 
-	local Reload = CreateItem("Button", Panel, true)
-	Reload:SetText("Press to reload menu.")
+	local Reload = Panel:FormButton("Press to reload menu.", "", "")
 	Reload.DoClick = function()
 		ACF_Conq.CreateContextPanel(Panel)
 	end
 
-	local Title = CreateItem("Help", Panel, true)
-	Title:SetText("Available options:")
+	Panel:FormLabel("Available options:")
 
-	local Options = CreateItem("DComboBox", Panel, true)
-	Options:SetSortItems(false)
+	local Options = Panel:FormComboBox()
 
-	local Tree = CreateItem("DTree", Panel)
+	local Tree = Panel:FormTree()
 	Tree.OnNodeSelected = function(_, Node)
 		if Option == Node then return end
 
 		Option = Node
 
-		ClearItems(Panel, "TempItems")
+		Panel:ClearItems("TempItems")
 
 		if Option.Action then
 			Option.Action(Panel)
@@ -204,7 +139,7 @@ local function CreateContextPanel(Panel)
 
 		Category = Data
 
-		ClearItems(Panel, "TempItems")
+		Panel:ClearItems("TempItems")
 
 		GenerateTree(Tree, Category)
 	end
@@ -217,56 +152,54 @@ AddOption("About the Addon")
 AddOptionItem("Updates", "icon16/newspaper.png", nil, WIPText)
 AddOptionItem("Description", "icon16/information.png", nil, WIPText)
 AddOptionItem("Report a Bug", "icon16/bug.png", nil, function(Panel)
-	local Text = CreateTempItem("Help", Panel, true)
-	Text:SetText("Found a bug and don't really want to fix it yourself?\n" ..
+	local Text = Panel:FormLabel(
+				"Found a bug and don't really want to fix it yourself?\n" ..
 				"Leave an issue report on the Github page!")
+	Text:SetTemporal(Panel)
 
-	local GitButton = CreateTempItem("Button", Panel, true)
-	GitButton:SetText("Report a Bug Now!")
+	local GitButton = Panel:FormButton("Report a Bug Now!")
+	GitButton:SetTemporal(Panel)
 	GitButton.DoClickInternal = function()
 		gui.OpenURL("https://github.com/TwistedTail/ACF-Conquest-Gamemode/issues/new/choose")
 	end
 
-	local Note = CreateTempItem("Help", Panel, true)
-	Note:SetText("Important: Problems are easier to solve if you know what caused them. " ..
+	local Note = Panel:FormLabel(
+				"Important: Problems are easier to solve if you know what caused them. " ..
 				"It's highly suggested to provide information about the bug you're reporting.")
+	Note:SetTemporal(Panel)
 
-	local Examples = CreateTempItem("Help", Panel, true)
-	Examples:SetText("Examples of 'useful information' would be:\n" ..
+	local Examples = Panel:FormLabel(
+					"Examples of 'useful information' would be:\n" ..
 					"- What were you doing when it happened?\n" ..
 					"- Was there an error on the console? If so, please provide it.\n" ..
 					"- Can you explain how to replicate the bug? If so, please give a short explanation.")
+	Examples:SetTemporal(Panel)
 end)
 
 AddOption("Tutorials")
 
 AddOption("Server Settings", true)
 AddOptionItem("Global Settings", "icon16/world.png", nil, function(Panel)
-	local GMEnable = CreateTempItem("CheckBox", Panel, true)
-	GMEnable:SetText("Enable the gamemode.")
-	GMEnable:SetConVar("acf_conquest_enable")
+	local GMEnable = Panel:FormCheckBox("Enable the gamemode.", "acf_conquest_enable")
+	GMEnable:SetTemporal(Panel)
 
-	local BotEnable = CreateTempItem("CheckBox", Panel, true)
-	BotEnable:SetText("Enable bot spawning.")
-	BotEnable:SetConVar("acf_conquest_enable_bots")
+	local BotEnable = Panel:FormCheckBox("Enable bot spawning.", "acf_conquest_enable_bots")
+	BotEnable:SetTemporal(Panel)
 
-	local BotAmount = CreateTempItem("NumSlider", Panel)
+	local BotAmount = Panel:FormNumSlider("Bots per team", "acf_conquest_max_bots")
+	BotAmount:SetTemporal(Panel)
 	BotAmount:SetTooltip("Defines the maximum amount of bots per team.")
-	BotAmount:SetText("Bots per team")
 	BotAmount:SetMax(100)
-	BotAmount:SetConVar("acf_conquest_max_bots")
 
-	local ScoreAmount = CreateTempItem("NumSlider", Panel)
+	local ScoreAmount = Panel:FormNumSlider("Tickets per team", "acf_conquest_max_tickets")
+	ScoreAmount:SetTemporal(Panel)
 	ScoreAmount:SetTooltip("Defines the amount of tickets each team starts with.")
-	ScoreAmount:SetText("Tickets per team")
 	ScoreAmount:SetMinMax(100, 2000)
-	ScoreAmount:SetConVar("acf_conquest_max_tickets")
 
-	local MinPlayers = CreateTempItem("NumSlider", Panel)
+	local MinPlayers = Panel:FormNumSlider("Minimum players", "acf_conquest_min_players")
+	MinPlayers:SetTemporal(Panel)
 	MinPlayers:SetTooltip("Defines the minimal amount of players needed to start a game.")
-	MinPlayers:SetText("Minimum players")
 	MinPlayers:SetMinMax(1, 30)
-	MinPlayers:SetConVar("acf_conquest_min_players")
 end)
 AddOptionItem("Map Settings", "icon16/map.png", nil, WIPText)
 
@@ -285,7 +218,5 @@ AddOptionItem("Edit Spawn Points", "icon16/pencil.png", nil, WIPText)
 
 -- Globalizing useful functions
 ACF_Conq.CreateContextPanel = CreateContextPanel
-ACF_Conq.CreateItem = CreateItem
-ACF_Conq.CreateTempItem = CreateTempItem
 ACF_Conq.AddOption = AddOption
 ACF_Conq.AddOptionItem = AddOptionItem
--- lua/acf_conquest/util.lua
+++ lua/acf_conquest/util.lua
@@ -19,4 +19,117 @@ local function CreateAccessor(Table, Key, Name, Type)
 	end
 end
 
+if CLIENT then
+	local PanelBase = baseclass.Get("Panel")
+
+	function PanelBase:SetTemporal(Parent)
+		if not Parent.TempItems then
+			Parent.TempItems = {}
+		end
+
+		Parent.TempItems[self] = true
+	end
+
+	timer.Simple(1, function()
+		function DForm:ClearItems(Name)
+			local Items = self[Name]
+
+			if Items and next(Items) then
+				for Item in pairs(Items) do
+					Item:Remove()
+				end
+			end
+
+			self[Name] = {}
+		end
+
+		function DForm:FormButton(Text, CVar, Vararg)
+			Text = Text or ""
+			CVar = CVar or ""
+			Vararg = Vararg or ""
+
+			local Item = self:Button(Text, CVar, Vararg)
+			Item:DockMargin(5, 0, 5, 0)
+			Item:SetFont("DermaDefaultBold")
+
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormCheckBox(Text, CVar)
+			Text = Text or ""
+			CVar = CVar or ""
+
+			local Item = self:CheckBox(Text, CVar)
+			Item:DockMargin(5, 0, 5, 0)
+			Item:SetFont("DermaDefaultBold")
+
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormLabel(Text)
+			Text = Text or ""
+
+			local Item = self:Help(Text)
+			Item:DockMargin(5, 0, 5, 0)
+			Item:SetFont("DermaDefaultBold")
+
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormTitle(Text)
+			Text = Text or ""
+
+			local Item = self:Help(Text)
+			Item:DockMargin(5, 0, 5, 0)
+			Item:SetFont("Trebuchet24")
+
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormComboBox()
+			local Item = vgui.Create("DComboBox", self)
+			Item:DockMargin(5, 0, 5, 0)
+			Item:SetFont("DermaDefaultBold")
+			Item:SetSortItems(false)
+			Item:SetDark(true)
+			Item:SetWrap(true)
+
+			self:AddItem(Item)
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormNumSlider(Text, CVar)
+			Text = Text or ""
+			CVar = CVar or ""
+
+			local Item = self:NumSlider(Text, CVar, 0, 0, 0)
+			Item:DockMargin(5, 0, 5, 0)
+
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+
+		function DForm:FormTree()
+			local Item = vgui.Create("DTree", self)
+			Item:DockMargin(5, 0, 5, 0)
+
+			self:AddItem(Item)
+			self.CreatedItems[Item] = true
+
+			return Item
+		end
+	end)
+end
+
 ACF_Conq.CreateAccessor = CreateAccessor
